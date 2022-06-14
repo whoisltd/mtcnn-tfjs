a6c0de76d849c013434bedf2315f4c5619046874
whoisltd <tiendat.le129@gmail.com>
2022-06-14 14:40:16 +0700

    update output, version

--- detect.js
+++ detect.js
@@ -5,7 +5,7 @@ var MTCNN = require('./mtcnn');
 const canvas = require('canvas');
 const path = require('path');
 
-async function detect(img_url, img_output, crop = false) {
+async function detect(img_url, draw = false, crop = false) {
     // """mtcnn image demo"""
     mtcnn = new MTCNN('file://' + path.join(__dirname, 'final_model/pnet/model.json'),
     'file://' + path.join(__dirname, 'final_model/rnet/model.json'), 
@@ -32,36 +32,27 @@ async function detect(img_url, img_output, crop = false) {
     const scores = data['scores'].arraySync()[0]
 
     // """Draw bounding boxes on image"""
-    canvas.loadImage(img).then(image => {
-        const ct = canvas.createCanvas(image.width, image.height)
-        const ctx = ct.getContext('2d');
-        
-        ctx.drawImage(image, 0, 0);
-        ctx.beginPath()
-        ctx.rect(boxes[0], boxes[1], boxes[2]-boxes[0], boxes[3]-boxes[1])
-        ctx.strokeStyle = 'red'
-        ctx.lineWidth = 4
-        ctx.stroke()
-        ctx.closePath()
+    if (draw){
+        canvas.loadImage(img).then(image => {
+            const ct = canvas.createCanvas(image.width, image.height)
+            const ctx = ct.getContext('2d');
+            
+            ctx.drawImage(image, 0, 0);
+            ctx.beginPath()
+            ctx.rect(boxes[0], boxes[1], boxes[2]-boxes[0], boxes[3]-boxes[1])
+            ctx.strokeStyle = 'red'
+            ctx.lineWidth = 4
+            ctx.stroke()
+            ctx.closePath()
 
-        if (img_output == null){
-            img_output = 'output.jpg'
-        }
-        var fileExt = img_output.split('.').pop();
-        var img_out = null
-        if (fileExt == 'jpg' || fileExt == 'jpeg') {
-            img_out = ct.toBuffer('image/jpeg')
-        } else if (fileExt == 'png') {
-            img_out = ct.toBuffer('image/png')
-        } else{
-            return "Error: unsupported image output format"
-        }
-
-        fs.writeFileSync(img_output, img_out)
-    })
+            var img_out = ct.toBuffer('image/jpeg')
+    
+            fs.writeFileSync('draw.jpg', img_out)
+        })
+    }
 
     // crop face
-    if (crop == true){
+    if (crop){
         for (let i = 0; i < 4; i++){
             boxes[i] = Math.round(boxes[i])
         }
